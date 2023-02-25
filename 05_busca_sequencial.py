686d289ee8c5399248e78df20ada909ab3e4348e
jpfalcuci <jpfalcuci@gmail.com>
2023-02-25 17:07:15 -0300

    feat: Testes com busca sequencial

--- 05_busca_sequencial.py
+++ 05_busca_sequencial.py
@@ -15,6 +15,7 @@ def busca_sequencial(lista, val):
     return -1
 
 # Busca sequencial: Lista precisa estar ordenada e quanto mais perto do fim o item estiver, mais tempo o algoritmo demora para encontrar
+# Só é eficiente se o item procurado estiver no começo da lista
 
 
 # TESTES
@@ -22,17 +23,51 @@ def busca_sequencial(lista, val):
 # Procurando o valor 15
 resultado = busca_sequencial(nums, 15)
 print(f'Posição do valor 15 na lista: {resultado}')
+print('\n', '-' * 80, '\n', sep='')
 
 # Procurando o valor 15
 resultado = busca_sequencial(nums, 20)
 print(f'Posição do valor 20 na lista: {resultado}')
+print('\n', '-' * 80, '\n', sep='')
 
-# Procurando o valor 15
+# Procurando o valor 33
 resultado = busca_sequencial(nums, 33)
 print(f'Posição do valor 33 na lista: {resultado}')
+print('\n', '-' * 80, '\n', sep='')
 
 
+# TESTES COM NOMES
+
+import sys
+sys.dont_write_bytecode = True # Impede a criação do cache
+
+from time import time
 from data.nomes_desord import nomes
 
+hora_ini = time()
 resultado = busca_sequencial(nomes, 'JOAO')
+hora_fim = time()
 print(f'Posição do valor "JOAO" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms')
+print('\n', '-' * 80, '\n', sep='')
+
+hora_ini = time()
+resultado = busca_sequencial(nomes, 'CARLOS')
+hora_fim = time()
+print(f'Posição do valor "CARLOS" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms')
+print('\n', '-' * 80, '\n', sep='')
+
+hora_ini = time()
+resultado = busca_sequencial(nomes, 'YARA')
+hora_fim = time()
+print(f'Posição do valor "YARA" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms')
+print('\n', '-' * 80, '\n', sep='')
+
+hora_ini = time()
+resultado = busca_sequencial(nomes, 'ORKUTILSON')
+hora_fim = time()
+print(f'Posição do valor "ORKUTILSON" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms')
+print('\n', '-' * 80, '\n', sep='')
