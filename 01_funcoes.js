a983d8c324f9e89e6d9ae04fd6a0ad9b068a59fc
jpfalcuci <jpfalcuci@gmail.com>
2023-02-25 17:45:05 -0300

    feat: Arredondamento de decimais com toFixed()

--- 01_funcoes.js
+++ 01_funcoes.js
@@ -6,8 +6,8 @@ function imc(peso, altura) {
 }
 
 const resultado = imc(81, 1.72);
-console.log('O IMC calculado é ', resultado);
-// O IMC calculado é  27.379664683612766
+console.log('O IMC calculado é ', resultado.toFixed(2));
+// O IMC calculado é  27.38
 
 console.log("\n" + "-".repeat(80) + "\n");
 
@@ -20,11 +20,11 @@ const pi = Math.PI;
 */
 function calcularArea(base, altura, tipo) {
   if (tipo === 'R') {           // Retângulo
-    return base * altura;
+    return (base * altura).toFixed(2);
   } else if (tipo === 'T') {    // Triângulo
-    return (base * altura) / 2;
+    return ((base * altura) / 2).toFixed(2);
   } else if (tipo === 'E') {    // Elipse
-    return (base / 2) * (altura / 2) * pi;
+    return ((base / 2) * (altura / 2) * pi).toFixed(2);
   } else {
     return null;
   }
@@ -32,8 +32,8 @@ function calcularArea(base, altura, tipo) {
 
 // Execução da função anteriormente declarada
 console.log('Área retângulo 10x25:', calcularArea(10, 25, 'R'));
-// Área retângulo 10x25: 250
+// Área retângulo 10x25: 250.00
 console.log('Área triângulo 12x7:', calcularArea(12, 7, 'T'));
-// Área triângulo 12x7: 42
+// Área triângulo 12x7: 42.00
 console.log('Área elipse 10x10:', calcularArea(10, 10, 'E'));
-// Área elipse 10x10: 78.53981633974483
+// Área elipse 10x10: 78.54
