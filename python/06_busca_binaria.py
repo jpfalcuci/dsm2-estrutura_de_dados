01fe9a23ac722b842f126a1afc7843164b2dc37d
jpfalcuci <jpfalcuci@gmail.com>
2023-03-04 17:04:34 -0300

    feat: Testes com  busca binária

--- python/06_busca_binaria.py
+++ python/06_busca_binaria.py
@@ -5,10 +5,11 @@
     Novas subdivisões são feitas até que se encontre o valor de busca 
     ou que reste apenas uma sub lista vazia, quando se conclui que o valor de busca não existe na lista
 """
-comps = 0       # Conta o número de comparações
 
-
+comps = 0               # Conta o número de comparações
 def busca_binaria(lista, val):
+    global comps
+    comps = 0
     ini = 0                 # Início da lista
     fim = len(lista) - 1    # Fim da lista
 
@@ -20,13 +21,47 @@ def busca_binaria(lista, val):
         if lista[meio] == val:
             comps += 1
             return meio
-
         elif val < lista[meio]:
             comps += 2
             fim = meio - 1
-
         else:
             comps += 2
-            fim = meio + 1
+            ini = meio + 1
 
     return -1   # Valor não existe na lista
+
+
+import sys
+sys.dont_write_bytecode = True # Impede a criação do cache
+
+from time import time
+from data.lista_nomes import nomes
+
+
+hora_ini = time()
+resultado = busca_binaria(nomes, 'JOAO')
+hora_fim = time()
+print(f'Posição do valor "JOAO" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms, comparações: {comps}')
+print('\n', '-' * 80, '\n', sep='')
+
+hora_ini = time()
+resultado = busca_binaria(nomes, 'CARLOS')
+hora_fim = time()
+print(f'Posição do valor "CARLOS" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms, comparações: {comps}')
+print('\n', '-' * 80, '\n', sep='')
+
+hora_ini = time()
+resultado = busca_binaria(nomes, 'YARA')
+hora_fim = time()
+print(f'Posição do valor "YARA" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms, comparações: {comps}')
+print('\n', '-' * 80, '\n', sep='')
+
+hora_ini = time()
+resultado = busca_binaria(nomes, 'ORKUTILSON')
+hora_fim = time()
+print(f'Posição do valor "ORKUTILSON" na lista: {resultado}')
+print(f'Tempo gasto: {round((hora_fim - hora_ini) * 1000, 2)}ms, comparações: {comps}')
+print('\n', '-' * 80, '\n', sep='')
