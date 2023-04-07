a7c1101d4e3775aeeb7188ab783bca1365752dd1
jpfalcuci <jpfalcuci@gmail.com>
2023-04-06 21:27:05 -0300

    feat: Pilhas em js

---
+++ js/17_pilha1.js
@@ -0,0 +1,48 @@
+/* 
+    Programa simples que inverte uma palavra e verifica se ela é um PALÍNDROMO, uma palavra que pode ser lida de trás para a frente, assim como de frente para trás.
+    Para isso, usaremos uma estrutura de pilha baseada em um array do JavaScript.
+*/
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// ovo, arara, omississimo
+rl.question("Informe a palavra a ser verificada: ", (palavra) => {
+    let pilha = []; // Lista vazia que será usada como pilha
+
+    // 1) Pega cada letra da palavra e insere no final (topo) da pilha
+    for (let letra of palavra) {
+        pilha.push(letra);
+        console.log(pilha);
+    }
+
+    console.log("*".repeat(50));
+
+    let inverso = "";
+
+    // 2) Vamos retirar as letras da pilha, uma a uma, DO FIM PARA O INÍCIO.
+    // A operação se repete enquanto a pilha não estiver vazia
+    // Cada letra retirada é acrescentada à variável inverso
+    while (pilha.length > 0) {
+        let letra = pilha.pop(); // Retira o último elemento da pilha
+        inverso += letra; // Acrescenta a letra ao inverso
+        console.log(`Pilha: ${pilha}; inverso: ${inverso}`);
+    }
+
+    console.log("*".repeat(50));
+
+    console.log("Palavra original: ", palavra);
+    console.log("Palavra invertida: ", inverso);
+
+    if (palavra === inverso) {
+        console.log("*** É UM PALÍNDROMO ***");
+    } else {
+        console.log("--- não é um palíndromo ---");
+    }
+
+    rl.close();
+});
---
+++ js/lib/stack.js
@@ -0,0 +1,64 @@
+/* 
+    ESTRUTURA DE DADOS PILHA
+    É uma estrutura de dados linear de acesso restrito na qual tanto a operação de inserção (tradicionalmente chamada "push"),
+    quanto a operação de remoção ("pop") acontecem no final (ou topo).
+    Em consequência, o funcionamento da pilha obedece ao princípio LIFO (Last In, First Out): o último elemento a entrar deve ser o primeiro a sair.
+*/
+
+class Stack {
+    constructor() {
+        // Cria uma lista privada e vazia para armazenar os dados da pilha
+        this._data = [];
+    }
+
+    // Método para inserção
+    // Em pilhas, tem nome padronizado: push
+    push(val) {
+        this._data.push(val);
+    }
+
+    // Método que verifica se pilha está ou não vazia
+    is_empty() {
+        return this._data.length == 0;
+    }
+
+    // Método para remoção
+    // Em pilha, tem nome padronizado: pop
+    pop() {
+        if (this.is_empty()) {
+            throw "ERRO: impossível remover de uma pilha vazia";
+        }
+
+        // Se chegou até aqui, a pilha NÃO está vazia e a remoção pode ser feita
+        return this._data.pop();
+    }
+
+    // Método que permite consultar o valor que está no topo da pilha, sem removê-lo
+    // Em pilhas, tem nome padronizado: peek
+    // ("Peek" significa "dar uma espiadinha" em inglês)
+    peek() {
+        if (this.is_empty()) {
+            throw "ERRO: impossível consultar uma pilha vazia";
+        }
+
+        return this._data[this._data.length - 1]; // Último elemento da lista
+    }
+
+    // Método que permite imprimir a lista como string
+    toString() {
+        return this._data.toString();
+    }
+}
+
+let pilha = new Stack(); // Cria uma pilha
+
+pilha.push("Primeiro");
+pilha.push("Segundo");
+pilha.push("Terceiro");
+
+console.log(pilha.toString());
+
+let removido = pilha.pop();
+console.log(`Removido: ${removido}`);
+
+console.log(pilha.toString());
